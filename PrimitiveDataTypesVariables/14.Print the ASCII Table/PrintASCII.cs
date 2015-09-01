baf0f6729430f708f67408e62bc3b7e9316ebf59
AlexanderKrustev <a.krustev@steni.bg>
2015-09-01 15:25:55 +0300

    PrimitiveDataTypeAndVariables

---
+++ PrimitiveDataTypesVariables/14.Print the ASCII Table/PrintASCII.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+class PrintASCII
+    {
+        static void Main(string[] args)
+        {
+        Console.OutputEncoding = Encoding.UTF8;
+        for ( int x = 0; x < 255; x++)
+        {
+            Console.WriteLine((char)x);
+        }
+        }
+    }
+
