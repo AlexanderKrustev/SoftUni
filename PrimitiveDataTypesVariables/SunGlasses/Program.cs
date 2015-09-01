1c4efef65e14e93a6c5ca9b1235bac5ade7a2f22
AlexanderKrustev <a.krustev@steni.bg>
2015-09-01 15:25:55 +0300

    PrimitiveDataTypeAndVariables

---
+++ PrimitiveDataTypesVariables/SunGlasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+   class Program
+    {
+        static void Main()
+        {
+        int height = int.Parse(Console.ReadLine());
+        int width = 2 * height;
+
+        Console.Write(new string('*', width));
+        Console.Write(new string(' ', height));
+        Console.Write(new string('*', width));
+        Console.WriteLine();
+        for(int x = 1; x < height-1; x++)
+        {
+            Console.Write(new string('*', 1));
+            Console.Write(new string('/', width-2));
+            Console.Write(new string('*', 1));
+            if(x== (height / 2 ))
+            {
+                Console.Write(new string('|', height));
+            }
+            else
+            {
+                Console.Write(new string(' ', height));
+            }
+            
+            Console.Write(new string('*', 1));
+            Console.Write(new string('/', width - 2));
+            Console.Write(new string('*', 1));
+            Console.WriteLine();
+        }
+        Console.Write(new string('*', width));
+        Console.Write(new string(' ', height));
+        Console.Write(new string('*', width));
+        Console.WriteLine();
+    }
+    }
+
