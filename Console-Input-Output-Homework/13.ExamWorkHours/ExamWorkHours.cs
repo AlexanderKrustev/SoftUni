14ab95a956600f050599114f23e3a0879e7ddfcb
AlexanderKrustev <a.krustev@steni.bg>
2015-09-12 13:50:24 +0300

    Console-Input-Output-Homework

---
+++ Console-Input-Output-Homework/13.ExamWorkHours/ExamWorkHours.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    class ExamWorkHours
+    {
+        static void Main(string[] args)
+        {
+        int h = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+        double percents = p * 0.01;
+        double workH = d - (d * 0.1);
+        workH = workH * 12;
+        workH = workH * percents;
+        int roundedWorkH = (int)workH;
+        if (h <= roundedWorkH)
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine(roundedWorkH-h);
+        }
+        else
+        {
+            Console.WriteLine("No");
+            Console.WriteLine(roundedWorkH-h);
+        }
+    }
+    }
+
---
+++ Console-Input-Output-Homework/14.ExamSumOfElements/SumOfElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SumOfElements
+    {
+        static void Main()
+        {
+        List<long> numbers = new List<long>();
+        string stringNumbers = Console.ReadLine();
+        string[] arrayString = stringNumbers.Split(' ');
+        foreach(string number in arrayString)
+        {
+            numbers.Add(int.Parse(number));
+        }
+        numbers.Sort();
+        int length = numbers.Count();
+        long maxNumber=numbers[length-1];
+        long sum = 0;
+        for(int x=0; x < length-1; x++)
+        {
+            sum += numbers[x];
+           
+        }
+        if (sum == maxNumber)
+        {
+            Console.WriteLine("Yes, sum={0}",sum);
+        }
+        else
+        {
+          
+            Console.WriteLine("No, diff={0}",Math.Abs(sum-maxNumber));
+
+        }
+        }
+    }
+
