6a59ee1a16b937e5e4b6c026454495114c734c5a
AlexanderKrustev <a.krustev@steni.bg>
2015-09-15 18:07:46 +0300

    Operators-Expressions-and-Statements

---
+++ Operators-Expressions-and-Statements/04. Rectangles/Rectangles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Rectangles
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Width= ");
+        double width = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Height= ");
+        double heigth = double.Parse(Console.ReadLine());
+        Console.WriteLine("Perimeter= {0} , Area= {1}",(2*width)+(2*heigth),width*heigth);
+    }
+    }
+
---
+++ Operators-Expressions-and-Statements/06. FourDigitNumber/FourDigitNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class FourDigitNumber
+    {
+        static void Main(string[] args)
+        {
+        int n = int.Parse(Console.ReadLine());
+        int firstDigit = n % 10;
+        int sedondDigit = (n / 10) % 10;
+        int thirdDigit = (n / 100)%10;
+        int fourthDigit = n / 1000;
+        Console.WriteLine("Sum={0}",firstDigit+sedondDigit+thirdDigit+fourthDigit);
+        Console.WriteLine("Reversed= {0}{1}{2}{3}", firstDigit,sedondDigit,thirdDigit,fourthDigit);
+        Console.WriteLine("Last digit in front = {0}{1}{2}{3}",firstDigit, fourthDigit,thirdDigit,sedondDigit);
+        Console.WriteLine("Second and third digits exchanged = {0}{1}{2}{3}", fourthDigit,sedondDigit,thirdDigit,firstDigit);
+    }
+    }
+
