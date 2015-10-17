969b71dbd19b547c38541e913a9407ccc03c6439
AlexanderKrustev <a.krustev@steni.bg>
2015-10-17 21:16:52 +0300

    CS Advanced

---
+++ CS Advanced/RemoveNames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Linq;
+
+    class RemoveNames
+    {
+        static void Main(string[] args)
+        {
+            string mainList = Console.ReadLine();
+            string names = Console.ReadLine();
+            string[] arr1 = mainList.Split(' ');
+            string[] arr2 = names.Split(' ');
+            
+            string result = "";
+            foreach(string s in arr1)
+            {
+                if (!arr2.Contains(s))
+                {
+                    result = result + " " + s;
+                }
+                
+            }
+            Console.WriteLine(result.Trim());
+            
+    
+        }
+    }
+}
---
+++ CS Advanced/SortingNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SortingNumbers
+    {
+        static void Main(string[] args)
+        {
+        List<int> number = new List<int>();
+        Console.Write("n=");
+        int n = int.Parse(Console.ReadLine());
+        for(int x=0; x< n; x++)
+        {
+            number.Add(int.Parse(Console.ReadLine()));
+        }
+        number.Sort();
+        foreach(int k in number)
+        {
+            Console.WriteLine(k);
+        }
+        }
+    }
+
