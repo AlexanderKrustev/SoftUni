688270bf82b2b348c42511f3b84345f404b18a45
AlexanderKrustev <a.krustev@steni.bg>
2015-11-23 11:03:27 +0200

    Exception Handling

---
+++ OOP/Exception Handling/01. Square Root/SquareRoot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Square_Root
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            double number = 0;
+            try
+            {
+                number= double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentException ();
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}",ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
---
+++ OOP/Exception Handling/02. Enter Numbers/EnterNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Enter_Numbers
+{
+    class EnterNumber
+    {
+        static void Main()
+        {
+
+            int startIndex = 1;
+            int endIndex = 100;
+
+            for(int x = 0; x < 10; x++)
+            {
+                ReadNumbers(startIndex,endIndex);
+            }
+        }
+
+        public static void ReadNumbers(int startIndex, int endIndex)
+        {
+
+            bool isIncorrect = true;
+            while (isIncorrect)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter number in range [{0}....{1}", startIndex, endIndex);
+                    double temp = double.Parse(Console.ReadLine());
+                    if (temp < startIndex || temp > endIndex)
+                    {
+                        throw new ArgumentException();
+                    }
+                    isIncorrect = false;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("The number should be in valid limits [{0}...{1}]", endIndex, startIndex);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Exception thrown {0}", ex.Message);
+                }
+            }
+        }
+    }
+}
